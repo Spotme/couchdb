4066ab38e0f02d91745968dc3a49ab27992618cf
AlexanderKaraberov <alexander@spotme.com>
2018-03-27 16:39:57 +0200

    fix(_changes): fix regression when parsing changes filter

--- src/chttpd/src/chttpd_db.erl
+++ src/chttpd/src/chttpd_db.erl
@@ -83,18 +83,18 @@ handle_changes_req(#httpd{path_parts=[_,<<"_changes">>]}=Req, _Db) ->
 handle_changes_req1(#httpd{}=Req, Db) ->
     #changes_args{filter=Raw, style=Style} = Args0 = parse_changes_query(Req),
     case couch_changes:configure_filter(Raw, Style, Req, Db) of
-      {FilterFun, []} ->
+      {FilterFun, ViewArgs} when is_list(ViewArgs), length(ViewArgs) > 0 ->
         ChangesArgs = Args0#changes_args{
               filter_fun = FilterFun,
+              filter_args = ViewArgs,
               db_open_options = [{user_ctx, couch_db:get_user_ctx(Db)}]
           };
-      {FilterFun, ViewArgs} ->
+      Filter ->
         ChangesArgs = Args0#changes_args{
-            filter_fun = FilterFun,
-            filter_args = ViewArgs,
+            filter_fun = Filter,
             db_open_options = [{user_ctx, couch_db:get_user_ctx(Db)}]
         }
-      end,
+    end,
     Max = chttpd:chunked_response_buffer_size(),
     case ChangesArgs#changes_args.feed of
     "normal" ->
