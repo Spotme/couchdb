96ff5b41c885752e417fade7bf6c902ea72b91d5
AlexanderKaraberov <alexander@spotme.com>
2018-05-24 17:06:01 +0200

    (fix view-filtered replication): make couchdb2 _last_seq compatible with rcouch's one

--- src/chttpd/src/chttpd_view.erl
+++ src/chttpd/src/chttpd_view.erl
@@ -81,7 +81,10 @@ handle_view_req(#httpd{method='GET',
         path_parts=[_, _, _, _, ViewName, <<"_last_seq">>]}=Req, Db, DDoc) ->
     {ok, GroupInfoList} = fabric:get_view_info(Db, DDoc, ViewName),
     LastSeq = proplists:get_value(update_seq, GroupInfoList, 0),
-    chttpd:send_json(Req, LastSeq);
+    chttpd:send_json(Req, {[
+        {name, ViewName},
+        {last_seq, LastSeq}
+    ]});
 
 handle_view_req(#httpd{method='GET',
     path_parts=[_, _, _, _, ViewName, <<"_info">>]}=Req, Db, DDoc) ->
